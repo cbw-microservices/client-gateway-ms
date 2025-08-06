6a234e6671aebb25de81e3f2966bc7ba18ec324d
luiscortesj1 <luiscortesj1999@gmail.com>
2025-08-06 17:52:41 -0500

    filtro de exception

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,11 @@
-import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, Patch, Post, Put, Query } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { TASK_SERVICE } from 'src/config';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { PatchTaskDto } from './dto/patch-task.dto';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom } from 'rxjs';
 @Controller('tasks')
 export class TasksController {
   constructor(
@@ -26,43 +26,57 @@ export class TasksController {
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    try {
-      return await firstValueFrom(this.tasksClient.send('findOneTask', { id }))
-    } catch (error) {
-      throw new BadRequestException(error)
-    }
+    return this.tasksClient.send('findOneTask', { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }));
   }
 
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksClient.send('updateTask', { updateTaskDto, id });
+  async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.tasksClient.send('updateTask', { updateTaskDto, id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }));;
   }
 
   @Patch(':id')
-  patch(@Param('id') id: string, @Body() patchTaskDto: PatchTaskDto) {
-    return this.tasksClient.send('patchTask', { id, patchTaskDto });
+  async patch(@Param('id') id: string, @Body() patchTaskDto: PatchTaskDto) {
+    return this.tasksClient.send('patchTask', { id, patchTaskDto }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }));;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tasksClient.send('removeTask', { id });
+  async remove(@Param('id') id: string) {
+    return this.tasksClient.send('removeTask', { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }));;
   }
 
   @Get('status/:status')
-  findByStatus(@Param('status') status: string) {
-    return this.tasksClient.send('findByStatus', status);
+  async findByStatus(@Param('status') status: string) {
+    return this.tasksClient.send('findByStatus', status).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }));
   }
 
   @Post(':id/schedule')
-  schedule(
+  async schedule(
     @Param('id') id: string,
     @Body() body: { runAt: Date },
   ) {
     return this.tasksClient.send('scheduleTask', {
       id,
       runAt: body.runAt,
-    });
+    }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }));
   }
 }
 
