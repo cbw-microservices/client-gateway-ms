8f4434eda7e201a91942f0d14362657b48c12771
luiscortesj1 <luiscortesj1999@gmail.com>
2025-08-07 14:11:43 -0500

    Add NATS module integration and update configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { NatsModule } from './transports/nats.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, NatsModule],
   controllers: [],
   providers: [],
 })
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
-
 import { TasksController } from './tasks.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs, NATS_SERVICE} from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [TasksController],
   providers: [],
   imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options:{
-         servers: envs.natsServers,
-        }
-      },
-  ])
-]
+    NatsModule
+  ]
 })
 export class TasksModule {}
