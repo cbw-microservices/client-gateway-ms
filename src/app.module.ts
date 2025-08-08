7bc95aae2b5c87f973a4165a5d844ea86f974e40
luiscortesj1 <luiscortesj1999@gmail.com>
2025-08-07 21:32:10 -0500

    Implement authentication module with user registration and login functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { NatsModule } from './transports/nats.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, NatsModule],
+  imports: [TasksModule, NatsModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { NATS_SERVICE } from 'src/config';
@@ -7,17 +7,20 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 import { PatchTaskDto } from './dto/patch-task.dto';
 import { catchError } from 'rxjs';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id.pipe';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 @Controller('tasks')
 export class TasksController {
   constructor(
     @Inject(NATS_SERVICE) private readonly tasksClient: ClientProxy,
   ) { }
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksClient.send('createTask', createTaskDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll(
     @Query() paginationDto: PaginationDto
@@ -25,6 +28,7 @@ export class TasksController {
     return this.tasksClient.send('findAllTasks', paginationDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   async findOne(@Param('id', ParseMongoIdPipe) id: string) {
     return this.tasksClient.send('findOneTask', { id }).pipe(
@@ -33,7 +37,7 @@ export class TasksController {
       }));
   }
 
-
+  @UseGuards(AuthGuard)
   @Put(':id')
   async update(@Param('id', ParseMongoIdPipe) id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksClient.send('updateTask', { updateTaskDto, id }).pipe(
@@ -41,7 +45,8 @@ export class TasksController {
         throw new RpcException(err);
       }));;
   }
-
+  
+  @UseGuards(AuthGuard)
   @Patch(':id')
   async patch(@Param('id', ParseMongoIdPipe) id: string, @Body() patchTaskDto: PatchTaskDto) {
     return this.tasksClient.send('patchTask', { id, patchTaskDto }).pipe(
@@ -50,6 +55,7 @@ export class TasksController {
       }));;
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id', ParseMongoIdPipe) id: string) {
     return this.tasksClient.send('removeTask', { id }).pipe(
@@ -58,6 +64,7 @@ export class TasksController {
       }));;
   }
 
+  @UseGuards(AuthGuard)
   @Get('status/:status')
   async findByStatus(@Param('status') status: string) {
     return this.tasksClient.send('findByStatus', status).pipe(
@@ -66,6 +73,7 @@ export class TasksController {
       }));
   }
 
+  @UseGuards(AuthGuard)
   @Post(':id/schedule')
   async schedule(
     @Param('id', ParseMongoIdPipe) id: string,
